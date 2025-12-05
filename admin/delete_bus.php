d05586df02c5db68da143b06d4c2d1acdc0162da
Diya2004-source <diyagajjar1220@gmail.com>
2025-12-05 08:55:27 +0530

    Delete admin/delete_bus.php

--- admin/delete_bus.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include "../db.php";
-if (!isset($_SESSION["admin"])) header("Location: admin_login.php");
-
-$id = $_GET["id"];
-$conn->query("DELETE FROM buses WHERE id=$id");
-
-echo "<script>alert('Bus deleted'); window.location='view_buses.php';</script>";
